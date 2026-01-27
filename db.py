913de0107a3a870fe7dacfbb8b0fa7c2ba0a1155
tishaker <tishakov.ilya.sergeevich@gmail.com>
2026-01-27 15:35:26 +0300

    .

--- db.py
+++ db.py
@@ -1,58 +1,20 @@
-import sqlite3
-from datetime import date
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
-def init_db():
-    conn = sqlite3.connect('admission.db')
-    cursor = conn.cursor()
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS programs (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        max_seats INTEGER NOT NULL
-    )
-    ''')
+class Applicant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    applicant_id = db.Column(db.Integer, unique=False)  # ID из CSV
+    consent = db.Column(db.Boolean, default=False)
+    priority = db.Column(db.Integer)  # 1-4
+    physics_score = db.Column(db.Integer)
+    russian_score = db.Column(db.Integer)
+    math_score = db.Column(db.Integer)
+    achievements_score = db.Column(db.Integer)
+    total_score = db.Column(db.Integer)
+    program = db.Column(db.String(10))  # ПМ, ИВТ, ИТСС, ИБ
+    date = db.Column(db.String(10))  # 01.08, 02.08 и т.д.
 
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS applicants (
-        id INTEGER PRIMARY KEY,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )
-    ''')
-
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS competition_lists (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        applicant_id INTEGER NOT NULL,
-        program_id INTEGER NOT NULL,
-        date DATE NOT NULL,  -- например '2025-08-01'
-        priority INTEGER CHECK (priority BETWEEN 1 AND 4),
-        physics_ict_score INTEGER,
-        russian_score INTEGER,
-        math_score INTEGER,
-        achievement_score INTEGER,
-        total_score INTEGER,
-        has_consent BOOLEAN DEFAULT 0,
-        FOREIGN KEY (applicant_id) REFERENCES applicants(id),
-        FOREIGN KEY (program_id) REFERENCES programs(id),
-        UNIQUE(applicant_id, program_id, date)  -- один абитуриент не может быть дважды в один день на одну программу
-    )
-    ''')
-
-    programs = [
-        ('ПМИ', 40),
-        ('ИВТ', 50),
-        ('ИТСС', 30),
-        ('ИБ', 20)
-    ]
-
-    cursor.execute("SELECT COUNT(*) FROM programs")
-    if cursor.fetchone()[0] == 0:
-        cursor.executemany("INSERT INTO programs (name, max_seats) VALUES (?, ?)", programs)
-
-    conn.commit()
-    conn.close()
-
-
-if __name__ == '__main__':
-    init_db()
+    def __repr__(self):
+        return f'<Applicant {self.applicant_id} {self.program} {self.date}>'
